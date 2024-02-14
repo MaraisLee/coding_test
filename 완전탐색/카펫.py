7bc929c785ca76ed7e3cb4a1d1898ff8cd1dffbd
Marais <maraisienne.lee@gmail.com>
2024-02-15 00:49:38 +0900

    fix: 완전탐색 카펫 주석 수정

--- 완전탐색/카펫.py
+++ 완전탐색/카펫.py
@@ -2,10 +2,11 @@
 
 def solution(brown, yellow):
     grid = brown + yellow
-    # 가로세로중 한면의 길이는 전체길이의 제곱근을 넘지 않음 
+    # 가로세로중 한면의 길이는 전체길이의 제곱근을 넘지 않음
+    # 즉, n은 항상 작은 쪽 
     for n in range(3, int(grid ** 0.5) + 1):
         if grid % n != 0:
             continue
         m = grid // n
         if (n - 2) * (m - 2) == yellow:
-            return sorted([m, n], reverse=True)
+            return [m, n]
